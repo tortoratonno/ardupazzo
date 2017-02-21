440ea812930bca741d45a0bddfad138473531c4b
stozacco <beppepunto@hotmail.com>
2017-02-21 22:44:24 +0100

    cuculo

--- sketches/tabbo_buzzer3/tabbo_buzzer3.ino
+++ sketches/tabbo_buzzer3/tabbo_buzzer3.ino
@@ -19,15 +19,28 @@
  *   - Pin dp to digital pin  9
  */
 
+char strMenu[4] = {'m','e','n','u'};
+char strTime[4] = {'t','i','m','e'};
+char strMelody[4] = {'s','o','n','g'};
+char strBuzz[4] = {'b','u','z','z'};
+
 SwitchButton buzzBtn(A0);
 SwitchButton startBtn(A1);
 SwitchButton setBtn(A2);  
-SwitchButton pulsanto(A5);  
+//SwitchButton pulsanto(A2);  
 
 const int buzzerPin = 2;
 
 bool play = false;
 short int durataShow;
+
+short int cnt = 0;
+int serpeLength = 7;
+//char serpe[4] = {'_','-','^','-'};
+char serpe[7] = {'p','u','p','p','a',' '};
+char buffy[4];
+int counter = 0;
+
 
 // IMPERIAL MARCH START
 
@@ -39,6 +52,7 @@ int noteDurationsImperial[] = {
   2, 2, 2, 3, 8, 2, 3, 8, 2
 };                                                                // note durations: 4 = quarter note, 8 = eighth note, etc.:
 
+                                            
 // IMPERIAL MARCH END
 
 int melody[] = {
@@ -52,25 +66,28 @@ int noteDurations[] = {
 SevenSegmentDisplay<true, QuadriDigit<4, 13, 3, 1> > ss(11, 10, 8, 6, 5, 12, 7, 9);
 
 void setup() {
-  //Serial.begin(9600);
+  // Serial.begin(9600);
   durataShow = 99;
 }
 
 void loop() {
+
   updateBtn();  
   sbuzzerazzizza();
-    
+  
+
+  
   if (startBtn.pressed() == true) {
     play = true;
     showTime(durataShow);
     finalTheme(melodyImperial, noteDurationsImperial);
+  } 
+  else if (setBtn.pressed() == true) {
+    menu();
+  } 
+  else {
+    serpentizza();
   }
-
-
-
-
-
-
 }
 
 
@@ -85,6 +102,12 @@ void loop() {
 */
 
 
+void updateBtn() {
+  buzzBtn.updateMe();
+  startBtn.updateMe();
+  setBtn.updateMe();
+  //pulsanto.updateMe();
+}
 
 
 void showTimeII(unsigned i){
@@ -115,11 +138,15 @@ void showTime(unsigned i){
 
 void finalTheme(int melodyArg[], int noteDurationsArg[]){
   for (int thisNote = 0; thisNote < 9; thisNote++) {
-    ss.print(8888, 2, 10);
-    ss.clear();
+    
+    char c[4] = {'H','E','L','L'};
+    
+    
     int noteDuration = 1000 / noteDurationsArg[thisNote];          //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     tone(buzzerPin, melodyArg[thisNote], noteDuration);
-    int pauseBetweenNotes = noteDuration * 1.30;
+    int pauseBetweenNotes = noteDuration * 1.25;                  //1.30
+    ss.print(c, -1, 27);
+    ss.clear();
     delay(pauseBetweenNotes);
   }
 }
@@ -135,7 +162,7 @@ void finalThemeORIG(int melodyArg[], int noteDurationsArg[]){
 }
 
 void sbuzzerazzizza(){
-  if (pulsanto.getCurrentState() > 300) {
+  if (buzzBtn.getCurrentState() > 300) {
     int noteDuration = 1000 / noteDurations[1];
     tone(buzzerPin, melody[0], noteDuration);
     }
@@ -145,14 +172,87 @@ void sbuzzerazzizza(){
   buzzBtn.updateMe();  
 }
 
-void updateBtn() {
-  buzzBtn.updateMe();
-  startBtn.updateMe();
-  setBtn.updateMe();
-  pulsanto.updateMe();
+void serpentizza(){
+  for (int j=0; j<=3; j++) {
+    buffy[j] = serpe[(j+cnt)%(serpeLength-1)];
+  }
+  ss.print(buffy, -1, 10);
+  counter += 1;
+  if (counter >= 15) {
+    cnt=(cnt+1)%(serpeLength-1);
+    counter = 0;
+  }
+}
+
+void menu() {
+    int cntMenu = 0;
+    char buff[4];
+    simpleFillBuff(buff, strMenu);
+    while (!buzzBtn.pressed()) {
+      ss.print(buff, -1, 10); 
+
+      if (startBtn.pressed() == true) {
+        cntMenu = (cntMenu + 1)%3+1;
+        switch(cntMenu)
+          {
+            case (1):
+                simpleFillBuff(buff, strTime);
+                break;
+                           
+            case (2):
+               simpleFillBuff(buff, strMelody);
+                break;
+         
+            case (3):
+                simpleFillBuff(buff, strBuzz);
+                break;
+          }
+      } 
+      else if (setBtn.pressed() == true) {
+
+        switch(cntMenu)
+          {
+            case (1):
+                setTime();
+                break;
+                           
+            case (2):
+                setMelody();
+                break;
+         
+            case (3):
+                setBuzzer();
+                break;
+          }
+      } 
+    }
+    
+    ss.clear();
+    delay(300);
 }
 
 void setTime() {
-  ss.print(12, 2, 10000);
+  while (!buzzBtn.pressed()) {
+  char c[4] = {'X','I','M','X'};
+  ss.print(c, -1, 10); 
+  }
 }
 
+void setMelody() {
+  while (!buzzBtn.pressed()) {
+  char c[4] = {'X','O','N','X'};
+  ss.print(c, -1, 10); 
+  }
+}
+void setBuzzer() {
+  while (!buzzBtn.pressed()) {
+  char c[4] = {'X','U','Z','X'};
+  ss.print(c, -1, 10); 
+  }
+}
+
+void simpleFillBuff(char *buff, char *strMenu) {
+  for (int j=0; j<=3; j++) {
+    buff[j] = strMenu[j];
+  }
+}
